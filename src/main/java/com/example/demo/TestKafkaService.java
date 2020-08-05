022d9066f044801e6ef62a93ad579bd154667f49
Rustam.Shavaliev <RRShavaliev@sberbank.ru>
2020-08-05 22:54:59 +0300

    impl demo

--- src/main/java/com/example/demo/TestKafkaService.java
+++ src/main/java/com/example/demo/TestKafkaService.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import brave.Span;
+import brave.Tracer;
+import brave.internal.codec.HexCodec;
+import brave.propagation.TraceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.stereotype.Service;
@@ -19,12 +24,28 @@ public class TestKafkaService {
 
     private final ReactiveKafkaConsumerTemplate<String, String> reactiveKafkaConsumerTemplate;
 
+    private final Tracer tracer;
+
     @PostConstruct
     public void toKafka() {
         Flux.range(0, Integer.MAX_VALUE)
                 .delayElements(Duration.ofSeconds(1))
-                .flatMap(integer -> reactiveKafkaProducerTemplate.send("test-topic", "test-value"))
-                .doOnNext(senderResult -> log.trace("senderResult = {}", senderResult))
+                .flatMap(integer ->
+                {
+                    TraceContext traceContext = TraceContext.newBuilder()
+                            .traceId(HexCodec.lowerHexToUnsignedLong("11111"))
+                            .spanId(HexCodec.lowerHexToUnsignedLong("22222"))
+                            .build();
+                    Span span = tracer.toSpan(traceContext);
+                    try {
+                        tracer.withSpanInScope(span);
+                        span.start();
+                        return reactiveKafkaProducerTemplate.send("test-topic", "test-value");
+                    } finally {
+                        span.finish();
+                    }
+                })
+                .doOnNext(senderResult -> log.error("senderResult = {}", senderResult))
                 .subscribeOn(Schedulers.boundedElastic())
                 .subscribe();
     }
